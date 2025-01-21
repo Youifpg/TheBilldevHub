37d1ca79e546e74481123cf3e072092ab78c5df0
ARBIX <youssefalsadick@gmail.com>
2025-01-21 23:25:59 +0300

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,1 +1,4 @@
-
+local players = game:GetService("Players") 
+local player = players.LocalPlayer 
+local teams = game:GetService("Teams")
+local replicatedStorage = game:GetService("ReplicatedStorage")
